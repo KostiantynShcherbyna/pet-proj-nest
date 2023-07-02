d9eb083f65007a0889a46d739f2816c7845daec9
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-07-02 20:40:37 +0300

    after install dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
+import dotenv from 'dotenv'
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+dotenv.config()
+const mongooseURI = process.env.MONGOOSE_URL || 'mongodb://0.0.0.0:27017'
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
