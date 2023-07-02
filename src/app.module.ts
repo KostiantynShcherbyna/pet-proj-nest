e2dd363d1821b29a05707b31d6081770c7abdbdf
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-07-02 20:42:16 +0300

    initialized commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 dotenv.config()
+
 const mongooseURI = process.env.MONGOOSE_URL || 'mongodb://0.0.0.0:27017'
 
 @Module({
