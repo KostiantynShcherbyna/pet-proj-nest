2f8ab84392094faf73d85a5a676f1f0290b7dd6e
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-08-04 12:33:35 +0300

    tests works

---
+++ src/app.settings.ts
@@ -0,0 +1,23 @@
+import { INestApplication, ValidationPipe } from "@nestjs/common"
+import cookieParser from "cookie-parser";
+import { errorsFactory } from "./infrastructure/adapters/exception-message.factory.adapter";
+import { ErrorExceptionFilter, HttpExceptionFilter } from "./infrastructure/filters/exeption.filter";
+import { useContainer } from "class-validator";
+import { AppModule } from "./app.module";
+
+export const appSettings = (app: INestApplication) => {
+    app.use(cookieParser());
+    app.enableCors()
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            stopAtFirstError: true,
+            exceptionFactory: errorsFactory
+        }),
+    )
+    app.useGlobalFilters(
+        new ErrorExceptionFilter(),
+        new HttpExceptionFilter()
+    )
+    useContainer(app.select(AppModule), { fallbackOnErrors: true })
+}
