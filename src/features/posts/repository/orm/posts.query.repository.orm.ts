4c01e5f6cb8e7b08643ea86cf97a84c8dbd3ff29
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-09-04 15:25:33 +0300

    posts works 22 ht

--- src/features/posts/repository/orm/posts.query.repository.orm.ts
+++ src/features/posts/repository/orm/posts.query.repository.orm.ts
@@ -45,7 +45,6 @@ export class PostsQueryRepositoryOrm {
 
     const pagesCount = Math.ceil(totalCount[0].count / pageSize)
 
-
     const posts = await this.dataSource.createQueryBuilder(PostEntity, "p")
       .select([
         `p.PostId as "postId"`,
@@ -59,8 +58,10 @@ export class PostsQueryRepositoryOrm {
         `pl.UserId as "userId"`,
         `pl.UserLogin as "userLogin"`
       ])
-      .addSelect(qb => this.likesCountBuilder(qb, "Like"), `likesCount`)
-      .addSelect(qb => this.likesCountBuilder(qb, "Dislike"), `disLikesCount`)
+      .addSelect(qb => this.likesCountBuilder1(qb), `likesCount`)
+      .addSelect(qb => this.likesCountBuilder2(qb), `dislikesCount`)
+      // .addSelect(qb => this.likesCountBuilder(qb, 'Like', "pl1"), `likesCount`)
+      // .addSelect(qb => this.likesCountBuilder(qb, 'Dislike', "pl2"), `dislikesCount`)
       .addSelect(qb => this.newestLikesBuilder(qb), `newestLikes`)
       .leftJoin(BlogEntity, "b", `b.BlogId = p.BlogId`)
       .leftJoin(PostLikeEntity, "pl", `pl.PostId = p.PostId and pl.UserId = :userId`, { userId })
@@ -69,70 +70,6 @@ export class PostsQueryRepositoryOrm {
       .limit(pageSize)
       .offset(offset)
       .getRawMany()
-    // console.log(posts.getSql())
-
-    // const newestLikes = await this.dataSource.createQueryBuilder(PostLikeEntity, "p")
-    //   .select([
-    //     `p.UserId as "userId"`,
-    //     `p.UserLogin as "login"`,
-    //     `p.AddedAt as "addedAt"`
-    //   ])
-    //   .leftJoin(BanInfoEntity, "b", `b.UserId = p.UserId`)
-    //   .where(`p.PostId = :postId`, { postId })
-    //   .andWhere(`p.Status = :likeStatus`, { likeStatus: "Like" })
-    //   .andWhere(`b.IsBanned = :isBanned`, { isBanned: false })
-    //   .orderBy(`p."AddedAt"`, sortDirection)
-    //   .limit(3)
-    //   .getRawMany()
-    //
-    //
-    // const queryForm = `
-    //     select a."PostId" as "postId", "Title" as "title", "ShortDescription" as "shortDescription", "Content" as "content",
-    //          "BlogName" as "blogName", a."BlogId" as "blogId", a."CreatedAt" as "createdAt",
-    //        (select "Status" from public."post_like_entity" where "PostId" = a."PostId" and "UserId" = $3) as "myStatus",
-    //        (select "UserId" from public."post_like_entity" where "PostId" = a."PostId" and "UserId" = $3) as "userId",
-    //        (select "UserLogin" from public."post_like_entity" where "PostId" = a."PostId" and "UserId" = $3) as "userLogin",
-    //        (
-    //         select count(*)
-    //         from public."post_like_entity" u
-    //         left join public."ban_info_entity" d on d."UserId" = u."UserId"
-    //         where u."Status" = 'Like'
-    //         and d."IsBanned" = 'false'
-    //         and a."PostId" = u."PostId"
-    //        ) as "likesCount",
-    //        (
-    //         select count(*)
-    //         from public."post_like_entity" u
-    //         left join public."ban_info_entity" d on d."UserId" = u."UserId"
-    //         where u."Status" = 'Dislike'
-    //         and d."IsBanned" = 'false'
-    //         and a."PostId" = u."PostId"
-    //        ) as "dislikesCount"
-    // from public."post_entity" a
-    // left join public."blog_entity" b on b."BlogId" = a."BlogId"
-    // where b."IsBanned" = 'false'
-    // order by a."${sortBy}" ${
-    //   sortBy !== "createdAt" ? "COLLATE \"C\"" : ""
-    // } ${sortDirection}
-    // limit $1
-    // offset $2
-    // `
-    //
-    // const newestLikesQueryForm = `
-    // select  a."UserId" as "userId", a."UserLogin" as "userLogin", a."AddedAt" as "addedAt", a."PostId" as "postId"
-    // from public."post_like_entity" a
-    // left join public."ban_info_entity" c on c."UserId" = a."UserId"
-    // where a."Status" = 'Like'
-    // and c."IsBanned" = 'false'
-    // order by "AddedAt" ${sortDirection}
-    // `
-    //
-    // const foundPosts = await this.dataSource.query(queryForm, [
-    //   pageSize, // 1
-    //   offset, // 2
-    //   userId // 3
-    // ])
-    // const foundNewestLikes = await this.dataSource.query(newestLikesQueryForm)
 
     const mappedPosts = this.changePostsView(posts)
 
@@ -237,25 +174,38 @@ export class PostsQueryRepositoryOrm {
     }
   }
 
-  private likesCountBuilder(qb: SelectQueryBuilder<any>, ls: string) {
+  private likesCountBuilder(qb: SelectQueryBuilder<any>, ls: string, alias: string) {
     return qb
       .select(`count(*)`)
-      .from(PostLikeEntity, "pl")
-      .leftJoin(BanInfoEntity, "b", `b.UserId = pl.UserId`)
-      .where(`pl.PostId = p.PostId`)
-      .andWhere(`pl.Status = :likeStatus`, { likeStatus: ls })
+      .from(PostLikeEntity, alias)
+      .leftJoin(BanInfoEntity, "b", `b.UserId = ${alias}.UserId`)
+      .where(`${alias}.PostId = p.PostId`)
+      .andWhere(`${alias}.Status = :likeStatus`, { likeStatus: ls })
+      .andWhere(`b.IsBanned = :isBanned`, { isBanned: false })
+  }
+
+  private likesCountBuilder1(qb: SelectQueryBuilder<any>) {
+    return qb
+      .select(`count(*)`)
+      .from(PostLikeEntity, "pl1")
+      .leftJoin(BanInfoEntity, "b", `b.UserId = pl1.UserId`)
+      .where(`pl1.PostId = p.PostId`)
+      .andWhere(`pl1.Status = 'Like'`)
+      .andWhere(`b.IsBanned = :isBanned`, { isBanned: false })
+  }
+
+  private likesCountBuilder2(qb: SelectQueryBuilder<any>) {
+    return qb
+      .select(`count(*)`)
+      .from(PostLikeEntity, "pl2")
+      .leftJoin(BanInfoEntity, "b", `b.UserId = pl2.UserId`)
+      .where(`pl2.PostId = p.PostId`)
+      .andWhere(`pl2.Status = 'Dislike'`)
       .andWhere(`b.IsBanned = :isBanned`, { isBanned: false })
   }
 
   private newestLikesBuilder(qb: SelectQueryBuilder<any>) {
-    return qb
-      // .select(`
-      // jsonb_agg(json_build_object('addedAt', to_char(agg.addedAt::timestamp at time zone 'UTC',
-      // 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"'), 'userId', cast(agg.userId as varchar), 'userLogin', agg.userLogin))
-      // `)
-      .select(`
-      json_agg(to_jsonb("agg")) as "newestLikes"
-      `)
+    return qb.select(`json_agg(to_jsonb("newestLikes")) as "newestLikes"`)
       .from(qb => {
         return qb
           .select([
@@ -271,21 +221,7 @@ export class PostsQueryRepositoryOrm {
           .groupBy(`ple.UserId, ple.UserLogin, ple.AddedAt`)
           .orderBy(`ple."AddedAt"`, "DESC")
           .limit(3)
-
-      }, "agg")
-
-    // .from(qb => {
-    //   return qb
-    //     // .select([`ple.AddedAt as "addedAt"`, `ple.UserId as "userId"`, `ple.UserLogin as "userLogin"`])
-    //     .select(`ple.AddedAt, ple.UserId, ple.UserLogin`)
-    //     .from(PostLikeEntity, "ple")
-    //     .leftJoin(BanInfoEntity, "bi")
-    //     .where(`ple.PostId = p.PostId`)
-    //     .andWhere(`ple.Status = :likeStatus`, { likeStatus: "Like" })
-    //     .andWhere(`bi.IsBanned = :isBanned and bi.UserId = ple.UserId`, { isBanned: false })
-    //     // .orderBy(`ple.AddedAt`, "DESC")
-    //     .limit(3)
-    // }, "agg")
+      }, "newestLikes")
   }
 
 
